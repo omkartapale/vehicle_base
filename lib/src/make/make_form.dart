dd08a65ee0bde508cdd350593692cd6ce802b336
Omkar Tapale <omkartapale@gmail.com>
2023-11-04 14:36:34 +0530

    Feat add forms for edit and create Make Add Delete dialog

---
+++ lib/src/make/make_form.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+
+import '../model/make.dart';
+
+class MakeForm extends StatefulWidget {
+  const MakeForm({super.key, this.makeItem});
+
+  final Make? makeItem;
+
+  @override
+  State<MakeForm> createState() => _MakeFormState();
+}
+
+class _MakeFormState extends State<MakeForm> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  bool _isCreate = true;
+
+  @override
+  void initState() {
+    super.initState();
+    if (widget.makeItem != null) {
+      setState(() {
+        _isCreate = false;
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      child: Form(
+        key: _formKey,
+        child: Column(
+          children: <Widget>[
+            TextFormField(
+              // keyboardType: TextInputType.text,
+              initialValue: widget.makeItem?.name,
+              decoration: InputDecoration(
+                isDense: true,
+                label: const Text('Manufacturer Name *'),
+                border:
+                    OutlineInputBorder(borderRadius: BorderRadius.circular(8)),
+                hintText: 'Enter Make name',
+              ),
+              validator: (String? value) {
+                if (value == null || value.trim().isEmpty) {
+                  return 'Please enter Manufacturer / Make name.';
+                }
+                if (value.trim().length < 2) {
+                  return 'Make should be atleast two characters long.';
+                }
+                return null;
+              },
+            ),
+            const SizedBox(height: 16),
+            FilledButton.icon(
+              onPressed: () {
+                // Validate will return true if the form is valid, or false if
+                // the form is invalid.
+                if (_formKey.currentState!.validate()) {
+                  // Process data.
+                  Navigator.pop(context);
+                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                      content: Text(
+                    'Manufacturer ${(_isCreate) ? 'created' : 'updated'}.',
+                    textAlign: TextAlign.center,
+                  )));
+                }
+              },
+              icon: const Icon(Icons.inventory_rounded),
+              label: (_isCreate)
+                  ? const Text('Add a Make')
+                  : const Text('Update Make'),
+            ),
+            const SizedBox(height: 16),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/src/make/make_list_view.dart
+++ lib/src/make/make_list_view.dart
@@ -4,6 +4,7 @@ import '../models/model_list_view.dart';
 import '../settings/settings_view.dart';
 import '../model/make.dart';
 import '../data/make_repository.dart';
+import 'make_form.dart';
 
 /// Displays a list of Vehicles.
 class MakeListView extends StatelessWidget {
@@ -60,14 +61,78 @@ class MakeListView extends StatelessWidget {
               // Display the Flutter Logo image asset.
               foregroundImage: AssetImage('assets/images/flutter_logo.png'),
             ),
+            onTap: () => _showBottomSheetForm(context, make),
+            onLongPress: () => _confirmDelete(context, make),
           );
         },
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () => _showBottomSheetForm(context),
         tooltip: 'Add maker',
         child: const Icon(Icons.factory),
       ),
+    );
+  }
+
+  Future<dynamic> _confirmDelete(BuildContext context, Make make) {
+    final ThemeData theme = Theme.of(context);
+    final TextStyle textStyle = theme.textTheme.bodyMedium!;
+    return showDialog(
+      context: context,
+      builder: (ctx) => AlertDialog(
+        title: Text("Delete ${make.name}?"),
+        content: RichText(
+          text: TextSpan(
+            children: <TextSpan>[
+              TextSpan(
+                  style: textStyle,
+                  text: 'Once you delete, you will not be able to use '
+                      'this in future. \n\nAre you sure to delete?'),
+              // TextSpan(
+              //     style: textStyle.copyWith(fontWeight: FontWeight.bold),
+              //     text: make.name),
+              // TextSpan(style: textStyle, text: '?'),
+            ],
+          ),
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.pop(ctx);
+            },
+            child: const Text('Cancel'),
+          ),
+          FilledButton(
+            onPressed: () {
+              Navigator.pop(ctx);
+              ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                  content: Text(
+                'Deleted ${make.name}.',
+                textAlign: TextAlign.center,
+              )));
+            },
+            child: const Text('Delete'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<void> _showBottomSheetForm(BuildContext context, [Make? make]) {
+    return showModalBottomSheet<void>(
+      context: context,
+      isScrollControlled: true,
+      showDragHandle: true,
+      builder: (context) {
+        return SingleChildScrollView(
+          child: Container(
+            padding: MediaQuery.of(context).viewInsets,
+            child: MakeForm(
+              makeItem: make,
+            ),
+          ),
+        );
+      },
     );
   }
 }
