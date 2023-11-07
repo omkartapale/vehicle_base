87ab0d271b007b8e1221624b1c6fc9b349a2f23c
Omkar Tapale <omkartapale@gmail.com>
2023-11-07 18:07:47 +0530

    Feat Filter UI and logic for Models

--- lib/src/models/model_list_view.dart
+++ lib/src/models/model_list_view.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:vehicle_base/src/make/make_list_view.dart';
 
+import '../data/make_repository.dart';
 import '../data/model_repository.dart';
+import '../make/make_list_view.dart';
+import '../model/make.dart';
 import '../model/model.dart';
 import '../settings/settings_view.dart';
 import 'model_form.dart';
@@ -12,26 +14,32 @@ class ModelListView extends StatefulWidget {
 
   static const routeName = '/models';
 
-  static List<Model> models = ModelRepository.loadAllModels();
-
   @override
   State<ModelListView> createState() => _ModelListViewState();
 }
 
 class _ModelListViewState extends State<ModelListView> {
+  final GlobalKey<FormState> _filterFormKey = GlobalKey<FormState>();
+  late List<Model> models;
   bool _isFiltered = false;
+  String? makeFilter;
+
+  @override
+  void initState() {
+    models = ModelRepository.loadAllModels();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Models'),
+        shadowColor: Theme.of(context).colorScheme.shadow,
         actions: [
           IconButton(
             onPressed: () {
-              setState(() {
-                _isFiltered = !_isFiltered;
-              });
+              _filterDialog(context);
             },
             icon: Icon(_isFiltered ? Icons.filter_alt_off : Icons.filter_alt),
             tooltip: 'Filter models',
@@ -62,31 +70,69 @@ class _ModelListViewState extends State<ModelListView> {
       // In contrast to the default ListView constructor, which requires
       // building all Widgets up front, the ListView.builder constructor lazily
       // builds Widgets as they’re scrolled into view.
-      body: ListView.builder(
-        // Providing a restorationId allows the ListView to restore the
-        // scroll position when a user leaves and returns to the app after it
-        // has been killed while running in the background.
-        restorationId: 'modelListView',
-        itemCount: ModelListView.models.length,
-        itemBuilder: (BuildContext context, int index) {
-          final model = ModelListView.models[index];
+      body: models.isEmpty
+          ? const Center(child: Text('No models found!'))
+          : ListView.builder(
+              // Providing a restorationId allows the ListView to restore the
+              // scroll position when a user leaves and returns to the app after it
+              // has been killed while running in the background.
+              restorationId: 'modelListView',
+              itemCount: models.length,
+              itemBuilder: (BuildContext context, int index) {
+                final model = models[index];
 
-          return ListTile(
-            title: Text(model.name),
-            subtitle: Text(model.make),
-            leading: const CircleAvatar(
-              // Display the Flutter Logo image asset.
-              foregroundImage: AssetImage('assets/images/flutter_logo.png'),
+                return ListTile(
+                  title: Text(model.name),
+                  subtitle: Text(model.make),
+                  leading: const CircleAvatar(
+                    // Display the Flutter Logo image asset.
+                    foregroundImage:
+                        AssetImage('assets/images/flutter_logo.png'),
+                  ),
+                  onTap: () => _showBottomSheetForm(context, model),
+                  onLongPress: () => _confirmDelete(context, model),
+                );
+              },
             ),
-            onTap: () => _showBottomSheetForm(context, model),
-            onLongPress: () => _confirmDelete(context, model),
-          );
-        },
-      ),
+
       floatingActionButton: FloatingActionButton(
         onPressed: () => _showBottomSheetForm(context),
         tooltip: 'Add model',
         child: const Icon(Icons.local_car_wash_rounded),
+      ),
+      floatingActionButtonLocation: _isFiltered
+          ? FloatingActionButtonLocation.endContained
+          : FloatingActionButtonLocation.endFloat,
+      bottomNavigationBar: AnimatedContainer(
+        duration: const Duration(milliseconds: 200),
+        height: (_isFiltered) ? 80 : 0,
+        child: BottomAppBar(
+          child: Card(
+            elevation: 0,
+            color: Colors.transparent,
+            margin: const EdgeInsets.only(left: 0, right: 72),
+            clipBehavior: Clip.hardEdge,
+            child: InkWell(
+              onTap: () => _filterDialog(context),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  IconButton(
+                    icon: const Icon(Icons.tune_rounded),
+                    onPressed: () => _filterDialog(context),
+                  ),
+                  const SizedBox(width: 8),
+                  Flexible(
+                    child: Text(
+                      '$makeFilter (${models.length})',
+                      style: Theme.of(context).textTheme.titleMedium,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
@@ -150,6 +196,68 @@ class _ModelListViewState extends State<ModelListView> {
           ),
         );
       },
+    );
+  }
+
+  Future<dynamic> _filterDialog(BuildContext context) {
+    final List<Make> makers = MakeRepository.loadAllMake();
+
+    return showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text("Filter Models"),
+        content: Form(
+          key: _filterFormKey,
+          child: DropdownButtonFormField(
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            value: _isFiltered ? makeFilter : null,
+            hint: const Text('Filter by Make'),
+            borderRadius: BorderRadius.circular(8),
+            items: makers.map((Make make) {
+              return DropdownMenuItem<String>(
+                value: make.name,
+                child: Text(make.name),
+              );
+            }).toList(),
+            onChanged: (value) {
+              makeFilter = value;
+            },
+            validator: (String? value) {
+              if (value == null || value.trim().isEmpty) {
+                return 'Please select Manufacturer.';
+              }
+              return null;
+            },
+          ),
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {
+              models = ModelRepository.loadAllModels();
+              setState(() {
+                makeFilter = null;
+                _isFiltered = false;
+              });
+              Navigator.pop(context);
+            },
+            child: const Text('Clear Filter'),
+          ),
+          FilledButton(
+            onPressed: () {
+              // Validate will return true if the form is valid, or false if
+              // the form is invalid.
+              if (_filterFormKey.currentState!.validate()) {
+                models = ModelRepository.filterModels(makeFilter!);
+                setState(() {
+                  _isFiltered = true;
+                });
+                Navigator.pop(context);
+              }
+            },
+            child: const Text('Filter'),
+          ),
+        ],
+      ),
     );
   }
 }
