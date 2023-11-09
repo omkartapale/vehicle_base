c1870893ef34077577346b18da52edce23116bdb
Omkar Tapale <omkartapale@gmail.com>
2023-11-09 17:58:46 +0530

    Firebase Init

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 import 'src/app.dart';
 import 'src/settings/settings_controller.dart';
 import 'src/settings/settings_service.dart';
 
 void main() async {
+// Initialize Firebase
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   // Set up the SettingsController, which will glue user settings to multiple
   // Flutter Widgets.
